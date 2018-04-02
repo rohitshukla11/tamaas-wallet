4f0f435c27550801101f1d6ebf16c986716fd727
rohitshukla11 <rohitshukla556@gmail.com>
2018-04-02 15:29:27 +0530

    bank api added

--- server.js
+++ server.js
@@ -20,10 +20,14 @@ app.use(function (req, res, next) {
 });
 
 //Setting up server
- var server = app.listen(process.env.PORT || 8080, function () {
+ var server = app.listen(process.env.PORT || 3000, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
  });
+
+ app.get('/', (req, res) => {
+  res.send('HEY!')
+})
 
 
 app.post("/createAccount", function(req , res){
@@ -67,7 +71,7 @@ app.post("/createAccount", function(req , res){
   }
 
 }, function(err, account) {
-  acct_id = account.id;
+  var acct_id = account.id;
   console.log("Account created successfully:"+acct_id);
 
      if (err){
@@ -78,9 +82,51 @@ app.post("/createAccount", function(req , res){
             var result = [];
             result.push({'status':'success',
                         'data':{'country': country, 'day': day, 'month': month, 'year': year, 'first_name': first_name, 'last_name': last_name,
-                        'address': address, 'postal_code': postal_code, 'city': city, 'state': state, 'ssn_last_4': ssn_last_4}
+                        'address': address, 'postal_code': postal_code, 'city': city, 'state': state, 'ssn_last_4': ssn_last_4, 'account_id': acct_id}
                     });
             res.json(result);
         } 
 });
+});
+
+app.post("/addBankAccount", function(req , res){
+var name = req.body.name;
+var routing_number = req.body.routing_number;          
+var account_number = req.body.account_number;
+var account_id = req.body.account_id;
+
+stripe.tokens.create({
+  bank_account: {
+    country: 'US',
+    currency: 'usd',
+    account_holder_name: String(name),
+    account_holder_type: 'individual',
+    routing_number: String(routing_number),
+    account_number: String(account_number)
+  }
+}, function(err, token) {
+  console.log(token['id']);
+  var token = token['id'];
+  stripe.accounts.createExternalAccount(
+  String(account_id),
+  { external_account: String(token) },
+  function(err, bank_account) {
+    bank_acct_id = bank_account.id;
+    console.log("Bank account added successfully:"+bank_acct_id);
+
+    if (err){
+            res.json({'status':'failed','message':'Error while fetching data'});
+            throw err;
+           }
+    else{
+            var result = [];
+            result.push({'status':'success',
+                        'data':{'account_holder_name': name,'routing_number': routing_number, 'account_number': account_number, 'bank_account_id': bank_acct_id }
+                    });
+            res.json(result);
+        } 
+  }
+);
+});
+
 });
